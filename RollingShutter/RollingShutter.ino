655a53de66aa4ac954309c3261e9b226e259c0cb
eric <emilletfr@gmail.com>
2016-11-26 15:51:59 +0100

    add vibration option

--- RollingShutter/RollingShutter.ino
+++ RollingShutter/RollingShutter.ino
@@ -5,30 +5,48 @@
 #include <ESP8266mDNS.h>
 #include <EEPROM.h>
 #include <Arduino.h>  // for type definitions
-#include <SimpleDHT.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-// for DHT11, 
+#define TEMPERATURE_MODULE_OR_VIBRATION_MODULE false
+IPAddress ip(10, 0, 1, 14); // 10.0.1.10 : Salon / 10.0.1.11 : Salle à manger / 10.0.1.12 : Bureau / 10.0.1.13 : Cuisine
+IPAddress gateway(10, 0, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+const char* ssid = "Airport Extreme";
+const char* password = "ENTER WIFI PASSWORD"; // ENTER WIFI PASSWORD !!!
+MDNSResponder mdns;
+
+#if TEMPERATURE_MODULE_OR_VIBRATION_MODULE == true
+
+#define DHTPIN 0         // Pin which is connected to the DHT sensor.
+// Uncomment the type of sensor in use:
+//#define DHTTYPE           DHT11     // DHT 11
+#define DHTTYPE           DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE           DHT21     // DHT 21 (AM2301)
+// See guide for details on sensor wiring and usage:
+//   https://learn.adafruit.com/dht/overview
+DHT_Unified dht(DHTPIN, DHTTYPE);
+// for DHT11,
 //      VCC: 5V or 3V
 //      GND: GND
 //      DATA: 2
-int pinDHT11 = 0;
-SimpleDHT11 dht11;
+//int pinDHT11 = 0;
+//SimpleDHT11 dht11;
+float temperature = 0;
+float humidity = 0;
 
+#else
 
+int EP = 0;
+int measurements[60];
+int indexMeasurement = 0;
+long measurementsSumMinute = 0;
+long measurementSeconde = 0;
 
-IPAddress ip(10, 0, 1, 10); // 10.0.1.10 : Salon / 10.0.1.11 : Salle à manger / 10.0.1.12 : Bureau / 10.0.1.13 : Cuisine
-IPAddress gateway(10, 0, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
-
-const char* ssid = "Airport Extreme";
-const char* password = "ENTER WIFI PASSWORD"; // ENTER WIFI PASSWORD !!! 
-MDNSResponder mdns;
-byte temperature = 0;
-byte humidity = 0;
-
+#endif
 
 ESP8266WebServer server(80);
-
 
 void handleStatusForHomebridge()
 {
@@ -61,25 +79,17 @@ void handleOpen()
 
 String jsonStatus ()
 {
-// read without samples.
-/*
-  temperature = 0;
-  humidity = 0;
-  if (dht11.read(pinDHT11, &temperature, &humidity, NULL)) {
-    Serial.print("Read DHT11 failed.");
-    temperature = 0;
-    humidity = 0;
-  }
-  */
-  Serial.print("Sample OK: ");
-  Serial.print((int)temperature); Serial.print(" *C, "); 
+#if TEMPERATURE_MODULE_OR_VIBRATION_MODULE == true
+  Serial.print((int)temperature); Serial.print(" *C, ");
   Serial.print((int)humidity); Serial.println(" %");
-
-
-  
-  //int statusNumber = getStatus() == true ? 1 : 0;
-  String msg = "{ \"open\": "; msg += getStatus();msg +=", \"temperature\": "; msg += (int)temperature ;msg +=", \"humidity\": "; msg += (int)humidity ; msg += "}";
+  String msg = "{ \"open\": "; msg += getStatus(); msg += ", \"temperature\": "; msg += temperature ; msg += ", \"humidity\": "; msg += humidity ; msg += "}";
   return msg;
+#else
+  Serial.print("measurementsSumMinute: "); Serial.print(measurementsSumMinute);
+  Serial.print(" - measurementSeconde: "); Serial.println(measurementSeconde);
+  String msg = "{ \"open\": "; msg += getStatus(); msg += ", \"measurementsSumMinute\": "; msg += measurementsSumMinute ; msg += ", \"measurementSeconde\": "; msg += measurementSeconde ; msg += "}";
+  return msg;
+#endif
 }
 
 void setStatus(bool status)
@@ -94,9 +104,11 @@ bool getStatus()
 {
   String msg = "restored status to : "; if (EEPROM.read(0) == 1) {
     msg += "1";
-  } else {
+  }
+  else {
     msg += "0";
-  } Serial.println(msg);
+  }
+  Serial.println(msg);
   return EEPROM.read(0) == 1 ? true : false;
 }
 
@@ -107,7 +119,7 @@ void setup(void)
   digitalWrite(05, LOW);
   digitalWrite(04, LOW);
 
-  Serial.begin(115200);
+  Serial.begin(9600);
   WiFi.config(ip, gateway, subnet);
   WiFi.begin(ssid, password);
 
@@ -136,19 +148,103 @@ void setup(void)
   EEPROM.begin(512);
   // EEPROM.end();
   Serial.printf("status restored to: %d\n", EEPROM.read(0));
+
+#if TEMPERATURE_MODULE_OR_VIBRATION_MODULE == true
+  // Initialize device.
+  dht.begin();
+  Serial.println("DHTxx Unified Sensor Example");
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Temperature");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");
+  Serial.println("------------------------------------");
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Humidity");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");
+  Serial.println("------------------------------------");
+#else
+  pinMode(EP, INPUT); //set EP input for measurment
+  for (int i = 0; i < 60; i++) {measurements[i] = 0;}
+#endif
 }
 
 void loop(void)
 {
   server.handleClient();
-  temperature = 0;
-  humidity = 0;
-  if (dht11.read(pinDHT11, &temperature, &humidity, NULL)) {
+
+#if TEMPERATURE_MODULE_OR_VIBRATION_MODULE == true
+  /*
+    temperature = 0;
+    humidity = 0;
+    if (dht11.read(pinDHT11, &temperature, &humidity, NULL)) {
     Serial.print("Read DHT11 failed.");
     temperature = 0;
     humidity = 0;
+    }
+  */
+  temperature = 0;
+  humidity = 0;
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+  else
+  {
+    temperature = event.temperature;
+    Serial.print("Temperature: ");
+    // Serial.print(event.temperature);
+    Serial.print(temperature);
+    Serial.println(" *C");
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity))
+  {
+    Serial.println("Error reading humidity!");
+  }
+  else
+  {
+    humidity = event.relative_humidity;
+    Serial.print("Humidity: ");
+    // Serial.print(event.relative_humidity);
+    Serial.print(humidity);
+    Serial.println("%");
   }
   delay(2000);
+#else
+  delay(50);
+  long measurement = pulseIn (EP, HIGH, 1000000); //wait for the pin to get HIGH and returns measurement
+  measurementSeconde = measurement;
+  if (measurement > 0) {
+    delay(1000);
+  }
+  indexMeasurement = indexMeasurement + 1; if (indexMeasurement == 60) {
+    indexMeasurement = 0;
+  }
+  measurements[indexMeasurement] = measurement;
+  measurementsSumMinute = 0;
+  for (int i = 0; i < 60; i++) {
+    measurementsSumMinute = measurementsSumMinute + measurements[i];
+  }
+  Serial.print("measurementsSumMinute: "); Serial.print(measurementsSumMinute);
+  Serial.print(" - measurementSeconde: "); Serial.println(measurementSeconde);
+#endif
 }
 
 void handleNotFound()
